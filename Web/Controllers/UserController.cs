a430800eae834de5d6e3f3995b2bb8f0e22e07f0
Gabriel Furrer <gfurrer@fyo.com>
2024-09-19 08:10:28 -0300

    Add project files.

---
+++ Web/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+
+        public JsonResult Get()
+        {
+            return new JsonResult(new List<User> {
+                                        new User { Id=1, Name= "Gabriel", LastName="Furrer", Password="1234", Email="hola@gmail.com" },
+                                        new User { Id=2, Name= "Carlos", LastName="Perez", Password="1234", Email="hola@gmail.com" },
+                                         new User { Id=3, Name= "Pedro", LastName="Rodriguez", Password="1234", Email="hola@gmail.com" }
+            });
+        }
+
+    }
+}
